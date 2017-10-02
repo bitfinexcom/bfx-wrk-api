7d893ceb6bfa00a97b30b007f31b61557e1ec193
prdn <paolo.ardoino@gmail.com>
2017-10-02 18:33:00 +0000

    fix

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ class WrkApi extends Base {
       ['fac', 'grc', 'p0', 'bfx', () => {
         return this.getGrcConf()
       }],
-      ['fac', 'api', 'bfx', 'bfx', () => {
+      ['fac', 'api/api', 'bfx', 'bfx', () => {
         return this.getApiConf()
       }]
     )
